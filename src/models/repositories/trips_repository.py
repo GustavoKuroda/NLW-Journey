474210e9302d72f3783c0f3e7c73fee12400b8f5
Gustavo Kenji Kuroda de Oliveira <guga.kenji1214@gmail.com>
2024-08-19 07:08:11 -0300

    feat: Implement db connection with repository

--- src/models/repositories/trips_repository.py
+++ src/models/repositories/trips_repository.py
@@ -1,10 +1,11 @@
+from typing import Dict, Tuple
 from sqlite3 import Connection
 
 class TripsRepository:
     def __init__(self, conn: Connection) -> None:
         self.__conn = conn
 
-    def create_trip(self, trip_infos: dict) -> None:
+    def create_trip(self, trip_infos: Dict) -> None:
         cursor = self.__conn.cursor()
         cursor.execute(
             '''
@@ -20,5 +21,25 @@ class TripsRepository:
                 trip_infos["owner_name"],
                 trip_infos["owner_email"]
             )
+        )
+        self.__conn.commit()
+
+    def find_trip_by_id(self, trip_id: str) -> Tuple:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''SELECT * FROM trips WHERE id = ?''', (trip_id,)
+        )
+        trip = cursor.fetchone()
+        return trip
+    
+    def update_trip_status(self, trip_id: str) -> None:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            '''
+                UPDATE trips
+                    SET status = 1
+                WHERE
+                    id = ?
+            ''', (trip_id,)
         )
         self.__conn.commit()
